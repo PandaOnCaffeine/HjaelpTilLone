d8b6a1baf1bc4c7f14c5a53b5aa440c48dc8736e
StationaryCaffeine <CaptPandaa@gmail.com>
2023-05-11 18:00:28 +0200

    Add project files.

---
+++ HjælpTilLone/IDieFightHeal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HjælpTilLone
+{
+    internal interface IDieFightHeal
+    {
+        void Die();
+        void Fight();
+        void Heal();
+    }
+}
---
+++ HjælpTilLone/IWizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HjælpTilLone
+{
+    internal interface IWizard : IDieFightHeal
+    {
+        void ThrowMagicMisile();
+        void ThrowFrostNova();
+        void Teleport(int x, int y);
+    }
+}
